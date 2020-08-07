d1480df3909011ce0a281b632c4a56fda06f09fc
Zachary Burk <zachburk7@gmail.com>
2020-08-07 09:56:15 -0400

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 module.exports = {
   purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
-  theme: {
-    extend: {},
-  },
-  variants: {},
   plugins: [],
+  important: false,
+  separator: ':',
+  theme: {
+    extend: {
+      height: {
+        '128': '32rem',
+      }
+    },
+  },
+  variants: {
+    appearance: ['responsive'],
+    zIndex: ['responsive'],
+  },
 }
