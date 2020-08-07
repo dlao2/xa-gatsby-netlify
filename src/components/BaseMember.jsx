97ad7f6389c0d0a47df79e4f765f44eb28850063
Zachary Burk <zachburk7@gmail.com>
2020-08-07 11:29:37 -0400

    Build staff page

---
+++ src/components/BaseMember.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+
+const BaseMember = (props) => {
+  const classObject = (borderColor) => {
+    var color = 'blue';
+    if (borderColor) {
+      color = borderColor;
+    }
+
+    return `p-1 rounded-full border-4 border-${ color }-400`;
+  };
+
+  return (
+    <div className={classObject(props.member.borderColor)} style={{width:'200px', height:'200px'}} onClick={props.onClick}>
+      <div
+        className="rounded-full staff-member bg-center"
+        style={{
+          backgroundImage: 'url(' + props.member.headshot + ')'
+        }}
+      >
+        <div className={ `opacity-50 h-full rounded-full ` + (props.hoverable ? 'lg:hover:bg-blue-500 cursor-pointer ' : ' ') +  (props.selectedMember === props.member ? 'lg:bg-blue-500': '') }>
+          <img src={ props.member.headshot } className="invisible" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BaseMember
---
+++ src/pages/staff.jsx
@@ -0,0 +1,103 @@
+import React, { useState } from "react"
+import { graphql } from "gatsby"
+import BaseBanner from "../components/BaseBanner"
+import BaseMember from "../components/BaseMember"
+
+const Staff = (props) => {
+  const data = props.data.allFile.edges[0].node.childMarkdownRemark.frontmatter
+
+  const [ selectedMember, setSelectedMember ] = useState(data.staff[0]);
+  return (
+    <div>
+      <div className="flex">
+        <div className="flex-1">
+          <BaseBanner
+            className="h-128"
+            text={ data.title }
+            img={ data.background_photo }
+          />
+        </div>
+      </div>
+
+      <div className="max-w-screen-xl flex flex-col lg:flex-row flex-wrap mx-auto justify-center">
+        {
+          data.staff.map(member => 
+            <div className="mt-20 m-10" key={member.name}>
+              <BaseMember
+                member={member}
+                selectedMember={selectedMember}
+                hoverable={true}
+                onClick={ () => setSelectedMember(member)}
+              />
+
+              <div className="lg:hidden">
+                <h2 className="text-3xl my-3">{ member.name }</h2>
+                {
+                  member.title
+                    && <h3 className="font-sans text-black text-xl my-3">
+                      { member.title }
+                    </h3>
+                }
+
+                <p className="leading-normal">{ member.biography }</p>
+              </div>
+            </div>
+          )
+        }
+      </div>
+
+      <div className="container mx-auto my-10 hidden lg:block">
+        <h2 className="text-4xl">{selectedMember.name }
+          {
+            selectedMember.title
+              && <span className="font-sans text-black text-2xl">
+                <span className="mx-4">|</span>
+                { selectedMember.title }
+              </span>
+          }
+        </h2>
+
+        <div className="flex flex-row my-10">
+          <p className="flex-grow leading-normal pr-6">{ selectedMember.biography }</p>
+          <div className="flex-none">
+            <img src={selectedMember.bio_photo} className="rounded" />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Staff
+
+export const pageQuery = graphql`
+query {
+  site {
+    siteMetadata {
+      title
+    }
+  }
+  allFile(filter: {name: {eq: "staff"}, sourceInstanceName: {eq: "content"}}) {
+    edges {
+      node {
+        id
+        relativePath
+        childMarkdownRemark {
+          html
+          frontmatter {
+            title
+            background_photo
+            staff {
+              name
+              position
+              headshot
+              biography
+              bio_photo
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
