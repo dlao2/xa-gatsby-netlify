5f0b5caf982485bbf920dfffb3b8bc7a37287fd9
Zachary Burk <zachburk7@gmail.com>
2020-08-07 21:42:49 -0400

    Remove PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,18 +63,6 @@ module.exports = {
         //trackingId: `ADD YOUR TRACKING ID HERE`,
       },
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
-      },
-    },
     `gatsby-plugin-react-helmet`,
     'gatsby-plugin-netlify-cms'
     // this (optional) plugin enables Progressive Web App + Offline functionality
