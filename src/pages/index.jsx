6884c156f80b39fa5f5abdbab1481af2b1ea0262
Zachary Burk <zachburk7@gmail.com>
2021-08-24 18:25:20 -0400

    Remove iframe

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,7 +10,7 @@ const Home = (props) => {
   return (
     <Layout>
       <SEO title={ data.title } />
-      <div className="flex lg:h-128 flex-col lg:flex-row">
+      <div className="flex flex-col lg:h-128 lg:flex-row">
         <div className="flex-1">
           <BaseBanner
             className="h-64 lg:h-full"
@@ -18,48 +18,42 @@ const Home = (props) => {
             img={data.heading_left.background_photo}
           />
         </div>
-        <div className="flex-1 bg-gray-900 h-full text-white">
+        <div className="flex-1 h-full text-white bg-gray-900">
           <GetConnectedForm />
         </div>
       </div>
 
-      <section className="py-20 bg-gray-200">
-        <div className="container mx-auto flex items-center justify-center">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/zi81EggAN1w" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-        </div>
-      </section>
-
       <section className="pb-20">
         <div className="container mx-auto">
-          <h2 className="text-center py-20 text-6xl">{ data.who_we_are.title }</h2>
-          <div className="flex flex-col lg:flex-row px-4 lg:px-0">
-            <div className="flex-1 lg:px-32 text-gray-900" dangerouslySetInnerHTML={{__html: data.who_we_are.text}}>
+          <h2 className="py-20 text-6xl text-center">{ data.who_we_are.title }</h2>
+          <div className="flex flex-col px-4 lg:flex-row lg:px-0">
+            <div className="flex-1 text-gray-900 lg:px-32" dangerouslySetInnerHTML={{__html: data.who_we_are.text}}>
             </div>
             <div className="flex-1">
-              <img src={ data.who_we_are.image } className="rounded mt-6 lg:-mt-4" />
+              <img src={ data.who_we_are.image } className="mt-6 rounded lg:-mt-4" />
             </div>
           </div>
         </div>
       </section>
 
-      <section className="py-20 bg-gray-200 flex flex-row justify-center text-blue-600 text-2xl leading-loose" dangerouslySetInnerHTML={{__html: data.section_3.text}}>
+      <section className="flex flex-row justify-center py-20 text-2xl leading-loose text-blue-600 bg-gray-200" dangerouslySetInnerHTML={{__html: data.section_3.text}}>
       </section>
 
-      <section className="py-20 flex flex-col items-center text-gray-900 text-lg leading-loose" dangerouslySetInnerHTML={{__html: data.section_4.text }}>
+      <section className="flex flex-col items-center py-20 text-lg leading-loose text-gray-900" dangerouslySetInnerHTML={{__html: data.section_4.text }}>
       </section>
 
       <section className="flex flex-col lg:flex-row lg:h-128">
-        <div className="flex-1 h-64 lg:h-full bg-center bg-cover" style={ { backgroundImage: 'url(' + data.life_groups.men.background_photo + ')' } }>
-          <div className="flex w-full h-full items-center justify-center">
+        <div className="flex-1 h-64 bg-center bg-cover lg:h-full" style={ { backgroundImage: 'url(' + data.life_groups.men.background_photo + ')' } }>
+          <div className="flex items-center justify-center w-full h-full">
             <Link to="/men">
-              <button className="bg-blue-500 p-4 rounded text-white hover:bg-blue-400">{ data.life_groups.men.text }</button>
+              <button className="p-4 text-white bg-blue-500 rounded hover:bg-blue-400">{ data.life_groups.men.text }</button>
             </Link>
           </div>
         </div>
-        <div className="flex-1 h-64 lg:h-full bg-center bg-cover" style={ { backgroundImage: 'url(' + data.life_groups.women.background_photo + ')' } }>
-          <div className="flex w-full h-full items-center justify-center">
+        <div className="flex-1 h-64 bg-center bg-cover lg:h-full" style={ { backgroundImage: 'url(' + data.life_groups.women.background_photo + ')' } }>
+          <div className="flex items-center justify-center w-full h-full">
             <Link to="/women">
-              <button className="bg-blue-500 p-4 rounded text-white hover:bg-blue-400">{ data.life_groups.women.text }</button>
+              <button className="p-4 text-white bg-blue-500 rounded hover:bg-blue-400">{ data.life_groups.women.text }</button>
             </Link>
           </div>
         </div>
